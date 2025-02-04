9871ce067375f59e0f0fe703e1e7adbb8ad8f762
Alice Chua <alice_chua@htx.gov.sg>
2025-02-04 14:51:12 +0800

    docker compose added

--- batch_CPU.Dockerfile
+++ batch_CPU.Dockerfile
@@ -1,21 +1,22 @@
-FROM python:3.12.8-alpine3.20
 # TODO: replace with speechmatics on-prem container as base
-# FROM batch-asr-transcriber-en:11.0.1
+FROM speechmatics-docker-demo.jfrog.io/sm-gpu-transcriber-batch-en-singapore_poc
 
 # set working directory, but according to speechmatics it can work from root
 # WORKDIR /opt/orchestrator
 
 # TODO: add python to speechmatics on-prem container
 RUN apt-get update
-RUN apt-get install -y python3 --no-cache-dir
+RUN apt-get install -y python3.12
 
 # install python dependencies
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install "fastapi[standard]"
 
-COPY ./config.json .
+COPY ./config.json /config.json
+# COPY ./license.json /license.json
 COPY ./main.py .
 
 EXPOSE 8000
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+ENTRYPOINT ["fastapi", "run", "main.py", "--port", "8000"]
--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
-TOKEN_VALUE = os.getenv("SPEECHMATICS_TOKEN")
-CONTAINER_NAME = os.getenv("SPEECHMATICS_CONTAINER_NAME")
+# LICENSE_TOKEN = os.getenv("LICENSE_TOKEN")
 # INPUT_FOLDER = os.getenv("INPUT_FOLDER") # commented out while we are using the root directory
 # SM_INFERENCE_ENDPOINT=os.getenv("SM_INFERENCE_ENDPOINT") # commented out until we get to using GPU support
 
+@app.get("/list_files")
+def list_files():
+    result = subprocess.run(["ls"], stdout=subprocess.PIPE).stdout.decode("utf-8")
+    return {"stdout": result}
 
 @app.post("/speechmatics_batch_wrapper")
 def speechmatics_batch_wrapper(File: UploadFile = File(...)):
@@ -20,9 +23,7 @@ def speechmatics_batch_wrapper(File: UploadFile = File(...)):
 
     # Run the docker container with the input file
     run_command = [
-        "pipeline",
-        f"LICENSE_TOKEN={TOKEN_VALUE}",
-    ]
+        "pipeline"]
     result = subprocess.run(run_command, stdout=subprocess.PIPE).stdout.decode("utf-8")
 
     # Clear the input file
