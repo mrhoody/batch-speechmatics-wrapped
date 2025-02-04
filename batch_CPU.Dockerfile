7e5d01e98e75c890d8cf03aa75b9760cd4d48d32
mrhoody <hud.syafiq@gmail.com>
2025-02-04 10:23:43 +0800

    modified dockerfile

--- batch_CPU.Dockerfile
+++ batch_CPU.Dockerfile
@@ -2,12 +2,20 @@ FROM python:3.12.8-alpine3.20
 # TODO: replace with speechmatics on-prem container as base
 # FROM batch-asr-transcriber-en:11.0.1
 
-WORKDIR /opt/orchestrator
+# set working directory, but according to speechmatics it can work from root
+# WORKDIR /opt/orchestrator
 
 # TODO: add python to speechmatics on-prem container
+RUN apt-get update
+RUN apt-get install -y python3 --no-cache-dir
 
 # install python dependencies
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
+
+COPY ./config.json .
+COPY ./main.py .
+
+EXPOSE 8000
 
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -7,32 +7,24 @@ app = FastAPI()
 
 TOKEN_VALUE = os.getenv("SPEECHMATICS_TOKEN")
 CONTAINER_NAME = os.getenv("SPEECHMATICS_CONTAINER_NAME")
-INPUT_FOLDER = os.getenv("INPUT_FOLDER")
+# INPUT_FOLDER = os.getenv("INPUT_FOLDER") # commented out while we are using the root directory
+# SM_INFERENCE_ENDPOINT=os.getenv("SM_INFERENCE_ENDPOINT") # commented out until we get to using GPU support
 
 
 @app.post("/speechmatics_batch_wrapper")
 def speechmatics_batch_wrapper(File: UploadFile = File(...)):
 
     # Save the uploaded file as input.audio as speechmatics requirements
-    with open(f"~/{INPUT_FOLDER}/input.audio", "wb") as f:
+    with open("./input.audio", "wb") as f:  # default to root directory for now
         f.write(File.file.read())
 
     # Run the docker container with the input file
     run_command = [
-        "docker",
-        "run",
-        "-i",
-        "-v",
-        f"~/{INPUT_FOLDER}:/input.audio",
-        "-e",
+        "pipeline",
         f"LICENSE_TOKEN={TOKEN_VALUE}",
-        f"{CONTAINER_NAME}",
     ]
+    result = subprocess.run(run_command, stdout=subprocess.PIPE).stdout.decode("utf-8")
 
-    # TODO: use actual command from speechmatics pipeline CLI
-    # result = subprocess.run("ls", stdout=subprocess.PIPE).stdout.decode("utf-8")
-
-    result = subprocess.run(run_command, stdout=subprocess.PIPE).stdout.decode(
-        "utf-8"
-    )  # placeholder command
+    # Clear the input file
+    os.remove("./input.audio")
     return {"stdout": result}
